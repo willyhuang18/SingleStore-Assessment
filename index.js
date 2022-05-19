4e9ddc1b797ff7b0b8ec557af9c4a1611d4f974b
Willy Huang <willyhuang18@gmail.com>
2022-05-19 00:49:26 -0700

    adding the validation form

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+const email = document.getElementById('email');
+
+function setError(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'input-box error';
+	small.innerText = message;
+}
+
+function setSuccess(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'input-box success';
+}
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Please make sure this not a personal email address');
+    } else {
+        setSuccess(email);
+    }
+
+};
+
+
 document.querySelectorAll('.custom-select').forEach(setupSelector);
 function setupSelector(selector) {
   selector.addEventListener('change', e => {
-    console.log('changed', e.target.value)
+    console.log('changed', e.target.value);
   })
 
   selector.addEventListener('mousedown', e => {
@@ -38,4 +70,25 @@ function setupSelector(selector) {
     }
   });
 }
- 
+
+function selectOption(){
+    let radio = document.forms[0];
+    let text = "";
+    let size = document.getElementById('size').value;
+    console.log(size);
+
+    for (let i = 0; i < radio.length; i++) {
+        if(radio[i].checked){
+            text += radio[i].value + "";
+        }    
+    }
+
+    if(text == "Price" || text == "Full Text Search" || text == "Document Storage" || size == "1-10"){
+        window.location.href = "./unqualified.html";
+    }else if(text == "" || size ==""){
+        window.alert("Please fill out the form!")
+    }else{
+        window.location.href = "./qualified.html";
+    }
+    validateInputs();
+}
